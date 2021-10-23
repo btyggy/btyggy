8172e27856e947a36671e4f192c21e9cec4cbbf2
btyggy <92939158+btyggy@users.noreply.github.com>
2021-10-23 02:12:04 +0100

    llhlhl

--- grid2.js
+++ grid2.js
@@ -75,19 +75,15 @@ const outflow = (mif, k) => {
 const getFlow = f => {
 	let [pr, prd, mif, mrr, rateFix] = ["pfr", "PerFlRaMD", "mif", "mrr", "rateFix"].map(getv)
 	let [id, mifa, k] = [d.activeElement.id, , kVal[mrr]]
-
 	if (id == "mrr" && rateFix == 1) {
 		mifa = pr * k
 		d.getElementById("mif").value = mifa.toFixed(2)
 		d.getElementById("PerFlRaMD").value = (24 * pr).toFixed(2)
 	}
-
 	if (id == "mif") outflow(mif, k)
-
 	if (id == "mrr") {
 		if (rateFix == 2 || rateFix == 0) outflow(mif, k)
 	}
-
 	//change to permeate flow rates
 	if (id == "pfr") {
 		prd = pr * 24
@@ -95,7 +91,6 @@ const getFlow = f => {
 		d.getElementById("PerFlRaMD").value = prd.toFixed(2)
 		d.getElementById("mif").value = mif.toFixed(2)
 	}
-
 	if (id == "PerFlRaMD") {
 		pr = prd / 24
 		mif = pr * k
@@ -166,20 +161,14 @@ const looky = () => {
 
 				c.log("chosen Pump = " + chosenPump)
 				// c.log("chosen Pump Line = " + chosenLine)
-
 				if (chosenLine.length != 0) {
-
 					d.getElementById("pum").innerText = pump
-
 					//set powercentre name
 					let [powCen, ERDLookUp, j, it] = [chosenPump[chosenPump.length - 1][10], chosenPump[0][membraneRecoveryRateVal + 17], 550, 0]
-
 					d.getElementById("powCen").innerText = powCen
-
 					//Pump Data
 					for (i = 11; i < 120; i = i + 3) {
 						j += 50
-
 						if (chosenLine[0][i] >= membrainInletFlowDay) {
 							pumpShaftSpeed = j
 							torque = chosenLine[0][i + 1]
@@ -198,17 +187,14 @@ const looky = () => {
 						cMonth: costPerDay * 31,
 						cYear: costPerDay * 365
 					}
-
 					cost.cDay = money + cost.cDay.toFixed(2).toString()
 					cost.cMonth = money + cost.cMonth.toFixed(2).toString()
 					cost.cYear = money + cost.cYear.toFixed(2).toString()
-
 					// this is the section for eleltric motor
 					d.getElementById("pkp").innerText = parseFloat(pkp).toFixed(2)
 					d.getElementById("cpd").innerText = cost.cDay
 					d.getElementById("cpm").innerText = cost.cMonth
 					d.getElementById("cpy").innerText = cost.cYear
-
 					pairs([
 						[pumpShaftSpeed, "PSS", " ", 2],
 						[torque, "tor", " ", 2],
@@ -216,7 +202,6 @@ const looky = () => {
 						[membrainOutputPressureFig, "pfpr", " ", 2],
 						[inletPress[chosenPump[chosenPump.length - 1][23]][it], "pfp", " ", 2]
 					])
-
 					c.log("Chosen Electric Motor Pump = " + emotorP(power))
 					c.log("Pump should be " + pump)
 					c.log("power centre is " + powCen)
@@ -232,13 +217,10 @@ const looky = () => {
 					c.log("Pump power kW " + power) // to be removed
 					c.log("inlet pressure " + chosenPump[chosenPump.length - 1][23]) // to be removed
 					c.log("ERD Look Up is " + ERDLookUp + " " + ERDmembraneRecoveryRate + " " + membraneInletPressure + " chosenPump[chosenPump.length - 1][22] " + chosenPump[chosenPump.length - 1][22])
-
 					//Motor Data
 					//                                                       size                          %                          %
 					let chosenMotor = ERDvals.filter(a => a[2] === ERDmembraneRecoveryRate && a[1] === ERDLookUp && a[3] === membraneInletPressure)
-
 					if (chosenMotor.length > 0) {
-
 						let chosenMotorPower = power - parseFloat(chosenMotor[0][i + 4])
 						let motor = chosenMotor[0][0]
 						let membrainOutputPressure = parseFloat(chosenMotor[0][4])
@@ -249,7 +231,6 @@ const looky = () => {
 						let pumpDrainFlow = (pumpVolumetricLoss + motorVolumetricLoss) / 24
 						let motorLoss = chosenLine[0][i + 2] + chosenMotor[0][11]
 						let motorTorque = chosenMotor[0][i + 3]
-
 						c.log("parseInt(membranePressureLoss) with cccc " + parseInt(membranePressureLoss))
 						c.log("power parseFloat(chosenMotor[0][11]) offSetMembrainOutputPressure[parseInt(membranePressureLoss)][0]")
 						c.log("sav = " + sav)
@@ -277,15 +258,12 @@ const looky = () => {
 							cMonth: costPerDayCostSaving * 31,
 							cYear: costPerDayCostSaving * 365
 						}
-
 						costSaving.cDay = money + costSaving.cDay.toFixed(2).toString()
 						costSaving.cMonth = money + costSaving.cMonth.toFixed(2).toString()
 						costSaving.cYear = money + costSaving.cYear.toFixed(2).toString()
-
 						d.getElementById("cpdCS").innerText = costSaving.cDay
 						d.getElementById("cpmCS").innerText = costSaving.cMonth
 						d.getElementById("cpyCS").innerText = costSaving.cYear
-
 						pairs([
 							[chosenMotorPower * offSetMembrainOutputPressure[parseInt(membranePressureLoss)][0], "saving", " ", 2],
 							[pumpVolumetricLoss / 24, "pl", " ", 2],
@@ -305,7 +283,6 @@ const looky = () => {
 						errorMes(2)
 						c.log("No motor Found")
 					}
-
 					// then the line is column filerted by the i postition +2 (80 becomes 82) this is then the first figure in the triptic
 					// i = the position in the array to manipulated to get the array elemnt in the second array(erd figs)
 				}
